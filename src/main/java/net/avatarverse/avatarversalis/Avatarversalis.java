d7d07ec7fe8e9954d709528f6bd1ccd45e9c13e2
George <54553504+gmrush@users.noreply.github.com>
2022-06-05 19:55:02 -0400

    Misc groundwork (#1)
    * Misc groundwork
    
    * Touchup

--- src/main/java/net/avatarverse/avatarversalis/Avatarversalis.java
+++ src/main/java/net/avatarverse/avatarversalis/Avatarversalis.java
@@ -1,19 +1,38 @@
 package net.avatarverse.avatarversalis;
 
+import java.util.Set;
+import java.util.logging.Logger;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.avatarverse.avatarversalis.core.Game;
+import net.avatarverse.avatarversalis.core.listener.UserListener;
 
 public class Avatarversalis extends JavaPlugin {
 
 	public static Avatarversalis plugin;
+	public static Logger log;
+	public static Game game;
+	private Set<Listener> listeners;
 
 	@Override
 	public void onEnable() {
 		plugin = this;
+		log = getLogger();
+		game = new Game();
+
+		listeners = Set.of(new UserListener());
+		listeners.forEach(l -> getServer().getPluginManager().registerEvents(l, this));
+
+		log.info("Avatarversalis has successfully been enabled.");
 	}
 
 	@Override
 	public void onDisable() {
-
+		listeners.forEach(HandlerList::unregisterAll);
+		log.info("Avatarversalis has successfully been disabled.");
 	}
 
 }
--- src/main/java/net/avatarverse/avatarversalis/core/board/BendingBoardManager.java
+++ src/main/java/net/avatarverse/avatarversalis/core/board/BendingBoardManager.java
@@ -1,4 +1,22 @@
 package net.avatarverse.avatarversalis.core.board;
 
-public class BendingBoardManager {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import net.avatarverse.avatarversalis.Avatarversalis;
+import net.avatarverse.avatarversalis.core.user.AvatarPlayer;
+
+/**
+ * Manager for every instance of {@link BendingBoard}
+ */
+
+public final class BendingBoardManager {
+
+    private final Map<AvatarPlayer, BendingBoard> playerBoards; //Maybe map UUIDS with the scoreboards instead?
+    private final boolean enabled = true; //Get this value from config later
+
+    public BendingBoardManager() {
+        playerBoards = new ConcurrentHashMap<>(); //Multithread safe!
+    }
+
 }
